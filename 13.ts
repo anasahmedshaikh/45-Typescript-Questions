b9ff9c6f0de2d113f0f7384d4bec647b5bd6b200
anasahmedshaikh <156065391+anasahmedshaikh@users.noreply.github.com>
2024-07-07 00:24:19 +0500

    Update 13.ts

--- 13.ts
+++ 13.ts
@@ -1,5 +1,5 @@
 // Task 13
 
-let favtransport : string [] = ["airplane", "car", "public bus", "ferrari",];
-favtransport.map((items) => console.log(`I like to travel in ${items}.`));
-console.log(`My favourate mode of transportation is ${favtransport[0]} because it is the fastest.`);
+let favTransport : string [] = ["airplane", "car", "public bus", "ferrari",];
+favTransport.map((items) => console.log(`I like to travel in ${items}.`));
+console.log(`My favourate mode of transportation is ${favTransport[0]} because it is the fastest.`);
