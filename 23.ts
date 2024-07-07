1113643cc35108f3ac2033be5393fd3b06419580
anasahmedshaikh <156065391+anasahmedshaikh@users.noreply.github.com>
2024-07-07 13:56:15 +0500

    Update 23.ts

--- 23.ts
+++ 23.ts
@@ -11,31 +11,31 @@ function runTests() {
     let isWeekend = false;
 
     // Test 1
-    console.log("Test 1: Is x less than y? I predict True.");
+    console.log("Test 1: Is x less than y? It should state True.");
     console.log(x < y);
 
     // Test 2
-    console.log("Test 2: Is name equal to 'John'? I predict True.");
+    console.log("Test 2: Is name equal to 'John'? It should state True.");
     console.log(name === "John");
 
     // Test 3
-    console.log("Test 3: Is age greater than or equal to 18? I predict True.");
+    console.log("Test 3: Is age greater than or equal to 18? It should state True.");
     console.log(age >= 18);
 
     // Test 4
-    console.log("Test 4: Is it raining? I predict True.");
+    console.log("Test 4: Is it raining? It should state True.");
     console.log(isRaining);
 
     // Test 5
-    console.log("Test 5: Is 'banana' included in fruits array? I predict True.");
+    console.log("Test 5: Is 'banana' included in fruits array? It should state True.");
     console.log(fruits.includes('banana'));
 
     // Test 6
-    console.log("Test 6: Is person's age less than 30? I predict False.");
+    console.log("Test 6: Is person's age less than 30? It should be False.");
     console.log(person.age < 30);
 
     // Test 7
-    console.log("Test 7: Is it morning? I predict False.");
+    console.log("Test 7: Is it morning? It should be False.");
     console.log(isMorning);
 
     // Test 8
@@ -43,11 +43,11 @@ function runTests() {
     console.log(isEvening);
 
     // Test 9
-    console.log("Test 9: Is x equal to y? I predict False.");
+    console.log("Test 9: Is x equal to y? It should be False.");
     console.log(x === y);
 
     // Test 10
-    console.log("Test 10: Is it weekend? I predict False.");
+    console.log("Test 10: Is it weekend? It should be False.");
     console.log(isWeekend);
 }
 
