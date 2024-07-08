9a6cb6a6c4d6fda75cfb7431ac6ade26682f18b7
anasahmedshaikh <156065391+anasahmedshaikh@users.noreply.github.com>
2024-07-08 14:47:40 +0500

    Update 34.ts

--- 34.ts
+++ 34.ts
@@ -1,10 +1,13 @@
 // Store pizza names in an array
 let pizzas: string[] = ['Pepperoni', 'Margherita', 'Supreme'];
 
+// Loop through the array and print the name of each pizza
+for (let pizza of pizzas) {
+    console.log(pizza);
+}
 // Loop through the array and print a sentence using the name of each pizza
 for (let pizza of pizzas) {
     console.log(`I like ${pizza} pizza.`);
 }
-
 // Print a line stating how much you like pizza
 console.log("I really love pizza!");
