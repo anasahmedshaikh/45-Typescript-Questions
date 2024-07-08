37f26c1af1534772e55982d03032f735fc0bb7dc
anasahmedshaikh <156065391+anasahmedshaikh@users.noreply.github.com>
2024-07-08 15:30:33 +0500

    Update 42.ts

--- 42.ts
+++ 42.ts
@@ -1,17 +1,19 @@
+// Array of magician's names
+const magicianNames: string[] = ["Harry potter", "Woldemont", "Scarlet Witch", "Ronn Wislee","Snape","Dumbeldoor"];
+
 // Define the show_magicians function
 function show_magicians(magicians: string[]) {
     for (const magician of magicians) {
         console.log(magician);
     }
 }
+// Call the show_magicians function
+show_magicians(magicianNames);
 
 // Define the make_great function
 function make_great(magicians: string[]) {
     return magicians.map(magician => `the Great ${magician}`);
 }
-
-// Array of magician's names
-const magicianNames: string[] = ["Junaid", "Harry ", "David ", "Jibran ", "Ahmed"];
 
 // Call make_great function to modify the array and then call show_magicians to display it
 show_magicians(make_great(magicianNames));
