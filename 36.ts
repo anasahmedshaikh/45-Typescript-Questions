79e2971c68f3d559f1d86206d01a0bb2d125181d
anasahmedshaikh <156065391+anasahmedshaikh@users.noreply.github.com>
2024-07-08 14:55:15 +0500

    Update 36.ts

--- 36.ts
+++ 36.ts
@@ -1,7 +1,7 @@
 // Function to make a shirt
-function make_shirt(size: string, message: string): void {
-    console.log(`Shirt size: ${size.toUpperCase()}, Message: ${message}`);
+function makeT_shirt (size: string, message: string): void {
+    console.log(`Shirt size: ${size.toUpperCase()}, This will be printed on your t-shirt ${message}`);
 }
 
 // Call the function
-make_shirt("medium", "Hello, World!");
+make_shirt("medium", "call of duty black oops");
