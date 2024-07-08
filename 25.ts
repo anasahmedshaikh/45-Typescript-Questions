aa52fc39e8ef13d35e24b05bb3c0e7ed5235a2a2
anasahmedshaikh <156065391+anasahmedshaikh@users.noreply.github.com>
2024-07-08 13:18:20 +0500

    Update 25.ts

--- 25.ts
+++ 25.ts
@@ -10,6 +10,6 @@ if (alien_color === 'green') {
 let alien_color1: string = 'red';
 
 // Failing version: alien_color is not 'green'
-if (alien_color === 'green') {
-    console.log("The player just earned 5 points.");
+if (alien_color !== 'green') {
+    return (failed)
 }
