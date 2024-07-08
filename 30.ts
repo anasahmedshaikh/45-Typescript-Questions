685cd5fdf1a086f50cd1be8db6ac1ef50de539b9
anasahmedshaikh <156065391+anasahmedshaikh@users.noreply.github.com>
2024-07-08 13:52:40 +0500

    Update 30.ts

--- 30.ts
+++ 30.ts
@@ -5,7 +5,13 @@ let usernames: string[] = ['admin', 'john', 'emma', 'alice', 'eric'];
 for (let username of usernames) {
     if (username === 'admin') {
         console.log("Hello admin, would you like to see a status report?");
-    } else {
+    } else if (username === 'john'){
+        console.log(`Hello ${username}, thank you for logging in again.`);
+    } else if (username === 'emma'){
+        console.log(`Hello ${username}, thank you for logging in again.`);
+    } else if (username === 'alice'){
+        console.log(`Hello ${username}, thank you for logging in again.`);
+    } else if (username === 'eric'){
         console.log(`Hello ${username}, thank you for logging in again.`);
     }
 }
