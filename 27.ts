2f2daa23814cf5a79760689b89b32c04597b0c78
anasahmedshaikh <156065391+anasahmedshaikh@users.noreply.github.com>
2024-07-08 13:31:46 +0500

    Update 27.ts

--- 27.ts
+++ 27.ts
@@ -1,32 +1,21 @@
-// Version 1 (alien_color is 'green'):
-let alien_color_1: string = 'green';
-
+let alien_color_1:string
+function alienColorTester (alien_color_1:string){
 if (alien_color_1 === 'green') {
     console.log("Version 1: The player earned 5 points.");
 } else if (alien_color_1 === 'yellow') {
-    console.log("Version 1: The player earned 10 points.");
-} else if (alien_color_1 === 'red') {
-    console.log("Version 1: The player earned 15 points.");
-}
-
-// Version 2 (alien_color is 'yellow'):
-let alien_color_2: string = 'yellow';
-
-if (alien_color_2 === 'green') {
-    console.log("Version 2: The player earned 5 points.");
-} else if (alien_color_2 === 'yellow') {
     console.log("Version 2: The player earned 10 points.");
-} else if (alien_color_2 === 'red') {
-    console.log("Version 2: The player earned 15 points.");
-}
-
-// Version 3 (alien_color is 'red'):
-let alien_color_3: string = 'red';
-
-if (alien_color_3 === 'green') {
-    console.log("Version 3: The player earned 5 points.");
-} else if (alien_color_3 === 'yellow') {
-    console.log("Version 3: The player earned 10 points.");
-} else if (alien_color_3 === 'red') {
+} else if (alien_color_1 === 'red') {
     console.log("Version 3: The player earned 15 points.");
 }
+}
+// Version 1 (alien_color is 'green'):
+alien_color_1 = 'green';
+alienColorTester(alien_color_1);
+
+// Version 2 (alien_color is 'yellow'):
+alien_color_1 = 'yellow';
+alienColorTester(alien_color_1);
+
+// Version 3 (alien_color is 'red'):
+alien_color_1 = 'red';
+alienColorTester(alien_color_1);
