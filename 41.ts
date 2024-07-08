f59fe3a5220e23a585fcdef46e695baf2d926c94
anasahmedshaikh <156065391+anasahmedshaikh@users.noreply.github.com>
2024-07-08 15:27:48 +0500

    Update 41.ts

--- 41.ts
+++ 41.ts
@@ -1,12 +1,11 @@
+// Array of magician's names
+const magicianNames: string[] = ["Harry potter", "Woldemont", "Scarlet Witch", "Ronn Wislee","Snape","Dumbeldoor"];
+
 // Define the show_magicians function
 function show_magicians(magicians: string[]) {
     for (const magician of magicians) {
         console.log(magician);
     }
 }
-
-// Array of magician's names
-const magicianNames: string[] = ["Merlin", "Harry ", "Jibran", "Ameen", "Tariq"];
-
 // Call the show_magicians function
 show_magicians(magicianNames);
